5bc1db40946a2265bd304783bd4152fc154406fe
jpaaquino <jpaaquino@hotmail.com>
2018-08-15 12:23:01 -0300

    Added like feature

--- Events/Controllers/EventTableViewCell.swift
+++ Events/Controllers/EventTableViewCell.swift
@@ -18,6 +18,8 @@ class EventTableViewCell: UITableViewCell {
     @IBOutlet weak var eventDatesLabel: UILabel!
     @IBOutlet weak var numberOfEventsLabel: UILabel!
     
+    var event:Event?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -30,32 +32,48 @@ class EventTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-    func addGradientToImage(){
-        let gradient = CAGradientLayer()
-        gradient.frame = eventImageView.bounds
-        let whiteGradient = UIColor(white: 0.0, alpha: 1.0).cgColor
-        let blackGradient = UIColor(white: 0.0, alpha: 0.1).cgColor
-        gradient.colors = [blackGradient, whiteGradient]
+    @IBAction func likeAction(_ sender: UIButton) {
+        guard let event = self.event else {return}
+        let defaults = UserDefaults.standard
+        var array = defaults.array(forKey: "favorites")  as? [Int] ?? [Int]()
+        if(array.contains(event.entityId)){
+            array = array.filter { $0 != event.entityId }
+        }else{
+            array.append(event.entityId)
+        }
+        defaults.set(array, forKey: "favorites")
+        updateHeartButton()
         
-        eventImageView.layer.insertSublayer(gradient, at: 0)
-
     }
     
     
     func configureCell(event:Event){
+        self.event = event
         self.artistNameLabel.text = event.topLabel
         self.eventLocationLabel.text = event.middleLabel
         self.eventImageView.image =  nil
-        self.heartButton.setImage(#imageLiteral(resourceName: "icons8-heart-white"), for: .normal)
+        //self.heartButton.setImage(#imageLiteral(resourceName: "icons8-heart-white"), for: .normal)
+        updateHeartButton()
         self.eventDatesLabel.text = event.bottomLabel
         self.numberOfEventsLabel.text = "\(event.eventCount) events ＞"
         
         Alamofire.request(event.image, method: .get).responseJSON { response in
             self.eventImageView.image = UIImage(data: response.data!, scale:1)
         }
-        
-   
-        
     }
-
+        
+        func updateHeartButton(){
+            guard let event = self.event else {return}
+            
+            let defaults = UserDefaults.standard
+            var array = defaults.array(forKey: "favorites")  as? [Int] ?? [Int]()
+            if(array.contains(event.entityId)){
+                heartButton.setImage(#imageLiteral(resourceName: "icons8-heart-red"), for: .normal)
+                
+            }else{
+                array.append(event.entityId)
+                heartButton.setImage(#imageLiteral(resourceName: "icons8-heart-white"), for: .normal)
+            }
+        }
+    
 }
